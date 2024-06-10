b33e525da9e60a94c7ccb39342bae2b1fda08217
Philipp Wüthrich <phiwut@users.noreply.github.com>
2024-06-10 15:28:44 +0200

    Theme update

--- src/components/SettingsModal.svelte
+++ src/components/SettingsModal.svelte
@@ -138,33 +138,21 @@
 					>
 						<option value="light">Light</option>
 						<option value="dark">Dark</option>
-						<option value="cupcake">Cupcake</option>
 						<option value="bumblebee">Bumblebee</option>
-						<option value="emerald">Emerald</option>
-						<option value="corporate">Corporate</option>
 						<option value="synthwave">Synthwave</option>
-						<option value="retro">Retro</option>
-						<option value="cyberpunk">Cyberpunk</option>
 						<option value="valentine">Valentine</option>
 						<option value="halloween">Halloween</option>
 						<option value="garden">Garden</option>
 						<option value="forest">Forest</option>
-						<option value="aqua">Aqua</option>
-						<option value="lofi">Lo-fi</option>
-						<option value="pastel">Pastel</option>
 						<option value="fantasy">Fantasy</option>
-						<option value="wireframe">Wireframe</option>
-						<option value="black">Black</option>
 						<option value="luxury">Luxury</option>
 						<option value="dracula">Dracula</option>
-						<option value="cmyk">CMYK</option>
 						<option value="autumn">Autumn</option>
-						<option value="business">Business</option>
-						<option value="acid">Acid</option>
-						<option value="lemonade">Lemonade</option>
 						<option value="night">Night</option>
 						<option value="coffee">Coffee</option>
-						<option value="winter">Winter</option>
+						<option value="dim">Dim</option>
+						<option value="nord">Nord</option>
+						<option value="sunset">Sunset</option>
 					</select>
 				</div>
 			{:else if activeTab === "info"}
